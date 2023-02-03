ef0036b39eaeb4bdca49a25977841cfe05e19e47
Naseem Ahmad <109871786+naseem12007571@users.noreply.github.com>
2023-02-04 00:55:28 +0530

    commit

---
+++ Reverse Array.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+ void reverse(int arr[],int size){
+     int start =0;
+     int end = size-1;
+     
+     while(start<=end){
+         swap(arr[start],arr[end]);
+         start++;
+         end--;
+         
+     }
+ }
+
+  void printArray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        cout<< arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int arr [6]= {4,7,8,9,2,1};
+    int brr [5]= {5,3,6,8,1};
+    
+    reverse(arr,6);
+    reverse(brr,5);
+    
+    printArray(arr,6);
+    printArray(brr,5);
+    
+    
+    
+    
+}
